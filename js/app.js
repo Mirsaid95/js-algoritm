0d4a9f70f54b3906e018232873402ca329e0f518
Mirsaid <akbaraliyev522@mail.ru>
2024-08-02 11:34:39 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// Lesson 1 
+// let num = 123;
+
+// let a = num % 100; 
+// let b = (num - a) / 100; 
+// let c = a % 10; 
+// let d = (a - c) / 10;
+
+// console.log(b);
+// console.log(d);
+// console.log(c);
+
+// console.log(c * 100 + b * 10 + d);
+
+
+
+// Lesson 2 
+// let num = 123;
+
+// let a = num % 100;
+// let b = (num - a) / 100; 
+// let c = a % 10; 
+// let d = (a - c) / 10; 
+
+// console.log(d * 100 + b * 10 + c);
+
+
+
+// Lesson 3 
+// let num = 123;
+
+// let a = num % 100;
+// let b = (num - a) / 100;
+// let c = a % 10; 
+// let d = (a - c) / 10;
+
+// console.log(b * 100 + c * 10 + d);
+
+
+
+// Lesson 4 
+// let num = 1234;
+
+// let a = num % 1000;
+// let b = a % 100;
+// let c = (a - b) / 100; 
+
+// console.log(c);
+
+
+
+// Lesson 5 
+// let num = 1234;
+
+// let a = num % 1000;
+// let b = (num - a) / 1000;
+// console.log(b);
